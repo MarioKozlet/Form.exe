8da4b166c8148315dad2d1c564bda63b23f33ec5
YuBruh <52368445+TSG-KUN@users.noreply.github.com>
2023-03-20 15:23:51 +0800

    ojk

--- resources/views/form/usermanage/usermanage.blade.php
+++ resources/views/form/usermanage/usermanage.blade.php
@@ -19,7 +19,7 @@
         <td> {{ $u->email }} </td> 
         <td style="text-transform: capitalize"> {{ $u->role }} </td>
         <td>
-            <button class="btn btn-warning"> <i class="ti ti-edit fs-3"></i> </button>
+            <button class="btn btn-warning" data-bs-toggle="modal" data-bs-target="#modalEdit{{ $u->id }}"> <i class="ti ti-edit fs-3"></i> </button>
             <button class="btn btn-danger" data-bs-toggle="modal" data-bs-target="#modalDelete{{ $u->id }}"> <i class="ti ti-trash fs-3"></i> </button>
         </td> 
     </tr>
@@ -43,6 +43,47 @@
         </div>
     </div>
 
+    <div class="modal fade" id="modalEdit{{ $u->id }}" tabindex="-1" data-bs-backdrop="static" data-bs-keyboard="false" role="dialog" aria-labelledby="modalTitleId" aria-hidden="true">
+        <div class="modal-dialog modal-dialog-scrollable modal-dialog-centered modal-lg" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="modalTitleId"> Edit User </h5>
+                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    <div class="modal-status"></div>
+                </div>
+                <div class="modal-body">
+                    <div class="row">
+                        <div class="col">
+                            <label class="fw-bold"> Email </label>
+                            <input type="email" name="email" value="{{ $u->email }}" class="form-control" @error('email') is-invalid @enderror>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label class="fw-bold"> Username </label>
+                            <input type="text" name="username" value="{{ $u->username }}" class="form-control" @error('username') is-invalid @enderror>
+                            <label class="text-muted"> Username dibuat untuk nama yang akan ditampilkan di website </label>
+                        </div>
+                        <div class="col">
+                            <label class="fw-bold"> Name </label>
+                            <input type="text" class="form-control" value="{{ $u->name }}" name="name" @error('name') is-invalid @enderror>
+                        </div>
+                    </div>
+                    <div class="row">
+                        <div class="col">
+                            <label class="fw-bold"> Password </label>
+                            <input type="password" class="form-control" name="password" @error('password') is-invalid @enderror>
+                            <label class="text-muted"> Password Tidak Bisa Diedit, tapi bisa dibuat ulang, karena password yg ada telah dihash untuk keamanaan akun </label>
+                        </div>
+                    </div>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary" data-bs-dismiss="modal"> Batal </button>
+                    <button type="submit" class="btn btn-success"> Update </button>
+                </div>
+            </div>
+        </div>
+    </div>
     @endforeach
     <form action="{{ route('register') }}" method="post">
         @csrf
